ec92611421124c4f8ccdd2daaeff8316d0a4b001
Rishikesh-177 <rushikesh.rikibe17@gmail.com>
2023-05-18 00:41:07 +0530

    routing assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
 import Taskbar from './Taskbar/Taskbar';
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
-import { Projects }   from './Taskbar/Pages/Projects';
-import  { Videos } from './Taskbar/Pages/Videos';
-import { About } from './Taskbar/Pages/About';
-import { Home } from './Taskbar/Pages/Home';
-import { Login } from './Taskbar/Pages/Login';
+import Projects   from './Taskbar/Pages/Projects';
+import  Videos from './Taskbar/Pages/Videos';
+import About from './Taskbar/Pages/About';
+import  Home from './Taskbar/Pages/Home';
+import Login from './Taskbar/Pages/Login';
 
 function App() {
   return (
@@ -16,18 +15,16 @@ function App() {
 <Taskbar /> 
 <div className='pages'>
 <Routes>
-  <Route exact path='/' Taskbar={Home}/>
-  <Route exact path='/about' Taskbar={About}/>
-  <Route exact path='/Projects' Taskbar={Projects}/>
-  <Route exact path='/videos' Taskbar={Videos}/>
-  <Route exact path='/login' Taskbar={Login}/>
+  <Route exact path='/' element={<Home />} />
+  <Route exact path='/about' element={<About />} />
+  <Route exact path='/Projects' element={<Projects />} />
+  <Route exact path='/videos' element={<Videos />} />
+  <Route exact path='/login' element={<Login />}  />
   </Routes>
-
-</div>
+  </div>
 </Router>
 </>
-   
-  );
+    );
 }
 
 export default App;
--- src/Taskbar/Taskbar.js
+++ src/Taskbar/Taskbar.js
@@ -3,49 +3,78 @@ import { NavLink  } from 'react-router-dom';
 import "./module.css";
 
 
+
 const Taskbar = () => {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
   return (
-    <nav className='"navbar'>
+
+    <nav className='navbar'>
  <div className='nav-container'>
   <NavLink exact to="/" className="nav-logo">
-  codeBucks
+    <div className='company-name'>
+  Lumbini <h6> Water Solution Pvt. ltd. </h6>
+  </div>
   <i className= "fas fa-code"></i>
     </NavLink> 
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li className='nav-item'>
-          <NavLink exact to="/" activeClassName='active' className="nav-links" 
-          onClick={handleClick}>
+          <NavLink
+           exact
+            to="/" 
+            activeClassName='active'
+             className="nav-links" 
+          onClick={handleClick}
+          >
 Home
           </NavLink>
         </li>
 
         <li className='nav-item'>
-          <NavLink exact to="/about" activeClassName='active' className='nav-links'
-          onClick={handleClick}>
+          <NavLink
+           exact 
+           to="/about" 
+           activeClassName='active'
+            className='nav-links'
+          onClick={handleClick}
+          >
 About
           </NavLink>
         </li>
 
         <li className='nav-item'>
-          <NavLink exact to="/projects" activeClassName='active'  className='nav-links'
-          onClick={handleClick}>
+          <NavLink
+           exact 
+           to="/projects" 
+           activeClassName='active'  
+           className='nav-links'
+          onClick={handleClick}
+          >
 Projects
           </NavLink>
         </li>
 
         <li className='nav-item'>
-          <NavLink exact to="/videos" activeClassName='active'  className='nav-links'
-          onClick={handleClick}>
+          <NavLink 
+          exact 
+          to="/videos"
+           activeClassName='active' 
+            className='nav-links'
+          onClick={handleClick}
+          >
 Videos
           </NavLink>
         </li>
 
         <li className='nav-item'>
-          <NavLink exact to="/login" activeClassName='active' className='nav-links'
-          onClick={handleClick}>
+          <NavLink 
+          exact 
+          to="/login" 
+          activeClassName='active' 
+          className='nav-links'
+          onClick={handleClick}
+          >
 Login
           </NavLink>
         </li>
@@ -56,6 +85,6 @@ Login
     </div>
     </nav>
   );
-}
+};
 
 export default Taskbar
