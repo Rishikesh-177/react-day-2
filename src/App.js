1a2f74720be2fbebe328deca2f6a7f9895ff1145
Rishikesh-177 <rushikesh.rikibe17@gmail.com>
2023-05-17 20:46:11 +0530

    routing assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Taskbar from './Taskbar/Taskbar';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
+import { Projects }   from './Taskbar/Pages/Projects';
+import  { Videos } from './Taskbar/Pages/Videos';
+import { About } from './Taskbar/Pages/About';
+import { Home } from './Taskbar/Pages/Home';
+import { Login } from './Taskbar/Pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <Router>
+<Taskbar /> 
+<div className='pages'>
+<Routes>
+  <Route exact path='/' Taskbar={Home}/>
+  <Route exact path='/about' Taskbar={About}/>
+  <Route exact path='/Projects' Taskbar={Projects}/>
+  <Route exact path='/videos' Taskbar={Videos}/>
+  <Route exact path='/login' Taskbar={Login}/>
+  </Routes>
+
+</div>
+</Router>
+</>
+   
   );
 }
 
---
+++ src/Taskbar/Taskbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { NavLink  } from 'react-router-dom';
+import "./module.css";
+
+
+const Taskbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <nav className='"navbar'>
+ <div className='nav-container'>
+  <NavLink exact to="/" className="nav-logo">
+  codeBucks
+  <i className= "fas fa-code"></i>
+    </NavLink> 
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className='nav-item'>
+          <NavLink exact to="/" activeClassName='active' className="nav-links" 
+          onClick={handleClick}>
+Home
+          </NavLink>
+        </li>
+
+        <li className='nav-item'>
+          <NavLink exact to="/about" activeClassName='active' className='nav-links'
+          onClick={handleClick}>
+About
+          </NavLink>
+        </li>
+
+        <li className='nav-item'>
+          <NavLink exact to="/projects" activeClassName='active'  className='nav-links'
+          onClick={handleClick}>
+Projects
+          </NavLink>
+        </li>
+
+        <li className='nav-item'>
+          <NavLink exact to="/videos" activeClassName='active'  className='nav-links'
+          onClick={handleClick}>
+Videos
+          </NavLink>
+        </li>
+
+        <li className='nav-item'>
+          <NavLink exact to="/login" activeClassName='active' className='nav-links'
+          onClick={handleClick}>
+Login
+          </NavLink>
+        </li>
+      </ul>
+      <div className="nav-icon" onClick={handleClick}>
+      <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+    </div>
+    </nav>
+  );
+}
+
+export default Taskbar
